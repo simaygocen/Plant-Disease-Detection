71a45ba767d0c92e65722f60f22d41e2f7c7267b
Elif Özker <111710264+elifozker@users.noreply.github.com>
2024-03-22 16:39:46 +0300

    app created

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Home from "./src/screens/HomeScreen";
+import Login from "./src/screens/LoginScreen";
+import SignUp from "./src/screens/SignUpScreen";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer independent={true}>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/SignUpScreen.js
@@ -0,0 +1,223 @@
+import {
+  View,
+  Text,
+  Image,
+  Pressable,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import COLORS from "../constants/colors";
+import { Ionicons } from "@expo/vector-icons";
+import Checkbox from "expo-checkbox";
+import Button from "../components/button";
+
+const SignUp = ({ navigation }) => {
+  const [isPasswordShown, setIsPasswordShown] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
+      <View style={{ flex: 1, marginHorizontal: 22 }}>
+        <View style={{ marginVertical: 10, marginBottom: 60 }}>
+          <Text
+            style={{
+              fontSize: 30,
+              fontWeight: "bold",
+              marginVertical: 12,
+              color: "black",
+              textAlign: "center",
+            }}
+          >
+            Get Started
+          </Text>
+
+          <Text
+            style={{
+              fontSize: 16,
+              color: COLORS.black,
+              fontWeight: "300",
+              textAlign: "center",
+            }}
+          >
+            by creating a free account.
+          </Text>
+        </View>
+
+        <View style={{ marginBottom: 20 }}>
+          <View
+            style={{
+              width: "100%",
+              height: 48,
+              borderColor: COLORS.black,
+              borderWidth: 1,
+              borderRadius: 8,
+              alignItems: "center",
+              justifyContent: "center",
+              paddingLeft: 22,
+            }}
+          >
+            <TextInput
+              placeholder="Full Name"
+              placeholderTextColor={COLORS.grey}
+              keyboardType="name-phone-pad"
+              style={{
+                width: "100%",
+              }}
+            />
+          </View>
+        </View>
+        <View style={{ marginBottom: 20 }}>
+          <View
+            style={{
+              width: "100%",
+              height: 48,
+              borderColor: COLORS.black,
+              borderWidth: 1,
+              borderRadius: 8,
+              alignItems: "center",
+              justifyContent: "center",
+              paddingLeft: 22,
+            }}
+          >
+            <TextInput
+              placeholder="E-mail address"
+              placeholderTextColor={COLORS.grey}
+              keyboardType="email-address"
+              style={{
+                width: "100%",
+              }}
+            />
+          </View>
+        </View>
+
+        <View style={{ marginBottom: 12 }}>
+          <View
+            style={{
+              width: "100%",
+              height: 48,
+              borderColor: COLORS.black,
+              borderWidth: 1,
+              borderRadius: 8,
+              alignItems: "center",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              paddingLeft: 22,
+            }}
+          >
+            <TextInput
+              placeholder="+90"
+              placeholderTextColor={COLORS.grey}
+              keyboardType="numeric"
+              style={{
+                width: "12%",
+                borderRightWidth: 1,
+                borderLeftColor: COLORS.grey,
+                height: "100%",
+              }}
+            />
+
+            <TextInput
+              placeholder="Phone Number"
+              placeholderTextColor={COLORS.grey}
+              keyboardType="numeric"
+              style={{
+                width: "80%",
+              }}
+            />
+          </View>
+        </View>
+
+        <View style={{ marginBottom: 12 }}>
+          <View
+            style={{
+              width: "100%",
+              height: 48,
+              borderColor: COLORS.grey,
+              backgroundColor: COLORS.grey,
+              borderWidth: 1,
+              borderRadius: 8,
+              alignItems: "center",
+              justifyContent: "center",
+              paddingLeft: 22,
+            }}
+          >
+            <TextInput
+              placeholder="Password"
+              placeholderTextColor={COLORS.black}
+              secureTextEntry={!isPasswordShown}
+              style={{
+                width: "100%",
+              }}
+            />
+
+            <TouchableOpacity
+              onPress={() => setIsPasswordShown(!isPasswordShown)}
+              style={{
+                position: "absolute",
+                right: 12,
+              }}
+            >
+              {isPasswordShown == true ? (
+                <Ionicons name="eye-off" size={24} color={COLORS.black} />
+              ) : (
+                <Ionicons name="eye" size={24} color={COLORS.black} />
+              )}
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            marginVertical: 6,
+          }}
+        >
+          <Checkbox
+            style={{ marginRight: 8 }}
+            value={isChecked}
+            onValueChange={setIsChecked}
+            color={isChecked ? COLORS.primary : undefined}
+          />
+
+          <Text>I aggree to the terms and conditions</Text>
+        </View>
+
+        <Button
+          title="Sign Up"
+          filled
+          style={{
+            marginTop: 18,
+            marginBottom: 4,
+          }}
+        />
+
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            marginVertical: 22,
+          }}
+        >
+          <Text style={{ fontSize: 16, color: COLORS.black }}>
+            Already have an account
+          </Text>
+          <Pressable onPress={() => navigation.navigate("Login")}>
+            <Text
+              style={{
+                fontSize: 16,
+                color: COLORS.primary,
+                fontWeight: "bold",
+                marginLeft: 6,
+              }}
+            >
+              Login
+            </Text>
+          </Pressable>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default SignUp;
