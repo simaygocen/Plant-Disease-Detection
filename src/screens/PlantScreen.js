287cc9758eeb4d4f95fb182b073611254e7808e1
Elif Özker <111710264+elifozker@users.noreply.github.com>
2024-04-25 18:19:36 +0300

    PlantScreen changed

--- src/screens/PlantScreen.js
+++ src/screens/PlantScreen.js
@@ -6,51 +6,78 @@ import {
   TouchableOpacity,
   ScrollView,
   Dimensions,
+  Image,
 } from "react-native";
-
+import { Ionicons } from "@expo/vector-icons";
 const { width, height } = Dimensions.get("window");
 
 const App = () => {
   const [activeFilter, setActiveFilter] = useState("All");
 
+  const plants = [
+    {
+      name: "Plant 1",
+      images: [require("../../assets/cedarr.jpg")],
+    },
+    {
+      name: "Plant 2",
+      images: [require("../../assets/blackrot.jpeg")],
+    },
+    {
+      name: "Plant 3",
+      images: [require("../../assets/scab.png")],
+    },
+  ];
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.headerTitle}>Healthy Plants</Text>
+        <Text style={styles.headerTitle}>{activeFilter} Plants</Text>
+        <TouchableOpacity style={styles.searchButton}>
+          <Image
+            style={styles.search}
+            source={require("../../assets/search.png")}
+          />
+        </TouchableOpacity>
       </View>
       <View style={styles.filterContainer}>
         <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-          {[
-            "All",
-            "Healthy",
-            "Apple Scab",
-            "Apple Black Rot",
-            "Apple Cedar",
-          ].map((filter) => (
-            <TouchableOpacity
-              key={filter}
-              style={[
-                styles.filterButton,
-                activeFilter === filter && styles.activeButton,
-              ]}
-              onPress={() => setActiveFilter(filter)}
-            >
-              <Text
+          {["All", "Healthy", "Apple Scab", "Black Rot", "Apple Cedar"].map(
+            (filter) => (
+              <TouchableOpacity
+                key={filter}
                 style={[
-                  styles.text,
-                  activeFilter === filter && styles.activeText,
+                  styles.filterButton,
+                  activeFilter === filter && styles.activeButton,
                 ]}
+                onPress={() => setActiveFilter(filter)}
               >
-                {filter}
-              </Text>
-            </TouchableOpacity>
-          ))}
+                <Text
+                  style={[
+                    styles.text,
+                    activeFilter === filter && styles.activeText,
+                  ]}
+                >
+                  {filter}
+                </Text>
+              </TouchableOpacity>
+            )
+          )}
         </ScrollView>
       </View>
       <ScrollView style={styles.plantsContainer}>
-        <Text>Plant 1</Text>
-        <Text>Plant 2</Text>
-        <Text>Plant 3</Text>
+        {plants.map((plant) => (
+          <View key={plant.name} style={styles.plantItem}>
+            {plant.images.map((image, index) => (
+              <Image
+                key={`${plant.name}_${index}`}
+                source={image}
+                style={styles.plantImage}
+              />
+            ))}
+            <Text style={styles.plantText}>{plant.name}</Text>
+          </View>
+        ))}
       </ScrollView>
     </View>
   );
@@ -64,27 +91,35 @@ const styles = StyleSheet.create({
   header: {
     flexDirection: "row",
     justifyContent: "space-between",
-    alignItems: "flex-start", // Changed from 'left' to 'flex-start'
+    alignItems: "center",
     paddingHorizontal: 0.05 * width,
     paddingTop: 0.2 * width,
   },
   headerTitle: {
-    fontSize: 0.045 * width,
+    fontSize: 0.055 * width,
     fontWeight: "bold",
     marginBottom: 0.012 * height,
+  },
+  searchButton: {
+    padding: 10,
+  },
+  search: {
+    width: 20,
+    height: 20,
   },
   filterContainer: {
     flexDirection: "row",
     marginVertical: 0.025 * width,
   },
   filterButton: {
+    marginRight: 0.025 * width,
     padding: 0.012 * height,
     borderWidth: 1,
     borderColor: "#eeeeee",
     borderRadius: 0.03 * width,
     backgroundColor: "#ececec",
-    left: 0.025 * width,
-    marginHorizontal: 0.01 * width,
+    left: 0.04 * width,
+    right: 0.04 * width,
   },
   activeButton: {
     backgroundColor: "#4CAF50",
@@ -92,11 +127,31 @@ const styles = StyleSheet.create({
   plantsContainer: {
     paddingHorizontal: 0.05 * width,
   },
+  plantItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#F0F0F0",
+    borderRadius: 25,
+    marginVertical: 8,
+    paddingVertical: 25,
+    paddingHorizontal: 15,
+  },
+  plantImage: {
+    width: 85,
+    height: 85,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  plantText: {
+    fontSize: 16,
+    color: "#333",
+    fontWeight: "500",
+  },
   text: {
     color: "#606470",
   },
   activeText: {
-    color: "#ffffff", // White color for active button text
+    color: "#ffffff",
   },
 });
 
